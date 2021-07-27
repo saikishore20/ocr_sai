ab919f20b6054492e0c75c598a32c1797cc8847a
saikishore20 <87608718+saikishore20@users.noreply.github.com>
2021-07-27 09:04:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,22 +12,18 @@ if pic is not None:
   st.image(img,caption='Uploaded Image')
   if st.button('DETECT'):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)          # Gray scale conversion
-      
     model = cv2.CascadeClassifier('/content/haarcascade_russian_plate_number.xml')
     demo = model.detectMultiScale(gray,1.1,10)
     for(x,y,w,h) in demo:
       g_roi = gray[y:y+h,x:x+w]
       roi = img[y:y+h,x:x+w]
     st.image(roi , caption = 'Number plate Detected')                     # Range of interest
-
     boxes = pytesseract.image_to_boxes(g_roi)             # Boxes
     #ha,wa,da =  roi.shape
     for e in boxes.splitlines():
        e=e.split()
        a,b,c,d = int(e[1]),int(e[2]),int(e[3]),int(e[4])
        rec = cv2.rectangle(roi,(a,b),(c,d),[250,0,0],2)
-       #st.write(boxes)
-    st.image(rec,caption = 'Image Boxes')                        # display Boxes
-                  
+    st.image(rec,caption = 'Image Boxes')                        # display Boxes            
     output = pytesseract.image_to_string(g_roi)
     st.write(f"Your Car number is {output}")             # Display string
